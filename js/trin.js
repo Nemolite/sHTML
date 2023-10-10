d7717deb0bfde962b395a2196f9f593fc62694e0
Nemolite <g25092011@mail.ru>
2023-10-10 08:06:41 +0300

    Новое

--- js/trin.js
+++ js/trin.js
@@ -1,1 +1,25 @@
 console.log(111)
+
+arr = new Array(1,21,32,43,5)
+let someDate = new Date("May 23, 2019")
+console.log(someDate)
+
+let s1 = new String("some text");
+let s2 = s1.substring(2);
+ 
+console.log(s2)
+s1.asr = "sert"
+
+console.log(s1.asr)
+
+arr2 = Array.of(5)
+console.log(arr2)
+
+for(let i = 0;i<=4;i++){
+    console.log(arr2[i])
+}
+
+
+for(let i of arr){
+    console.log(i)
+}
