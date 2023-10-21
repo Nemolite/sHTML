f28d0b320769eefb3d10690b5ba3bffa8e42c8df
Nemolite <g25092011@mail.ru>
2023-10-21 06:28:12 +0300

    client api

--- js/trin.js
+++ js/trin.js
@@ -1,32 +1,24 @@
-console.log(111)
-
-arr = new Array(1,21,32,43,5)
-let someDate = new Date("May 23, 2019")
-console.log(someDate)
-
-let s1 = new String("some text");
-let s2 = s1.substring(2);
- 
-console.log(s2)
-s1.asr = "sert"
-
-console.log(s1.asr)
-
-arr2 = Array.of(5)
-console.log(arr2)
-
-for(let i = 0;i<=4;i++){
-    console.log(arr2[i])
-}
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
 
 
-for(let i of arr){
-    console.log(i)
-}
-
-let arr5 = [3,4,56,6]
-
-let arr7 = [...arr5]
-console.log(arr7)
-
-
+      async function getUsers() {
+        // отправляет запрос и получаем ответ
+        const response = await fetch("https://jsonplaceholder.typicode.com/users", {
+            method: "GET",
+            headers: { "Accept": "application/json" }
+        });
+        // если запрос прошел нормально
+        if (response.ok === true) {
+            // получаем данные
+            const users = await response.json();
+            console.log(users)
+            let rows = document.querySelector("body"); 
+            users.forEach(user => {
+                // добавляем полученные элементы в таблицу
+                rows.append(row(user));
+            });
+        }
+    }
+    getUsers()
